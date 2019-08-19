18a454180510505d2cf9ea56bee1f476272e3fa4
Exprescode <jinghui.yew@gmail.com>
2019-08-19 14:16:19 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String BAR = "\t____________________________________________________________";
+    /**
+     * Program starts here. This is the main method.
+     * @param args cmd line input arguments.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        print_init_msg();
+        String user_input = scan.nextLine();
+        while(!user_input.equals("bye")){
+            print_console(user_input);
+            user_input = scan.nextLine();
+        }
+        print_exit_msg();
+    }
+
+    private static void print_console(String msg) {
+        System.out.println(BAR + "\n\t " + msg.replaceAll("\n", "\n\t ") + "\n" + BAR + "\n");
+    }
+
+    private static void print_init_msg() {
+        print_console("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private static void print_exit_msg() {
+        print_console("Bye. Hope to see you again soon!");
     }
 }
